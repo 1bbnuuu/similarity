6c19d9c446a4e1194c8d4c8065513d272c200316
1bbnuuu <ibnuzzy29@gmail.com>
2025-11-27 23:31:02 +0700

    first commit

--- PREPROCESSING/casefolding.js
+++
@@ -1,8 +0,0 @@
-const judul = "Simulator Pemrosesan Citra Menggunakan Metode Konvolusi Untuk Gambar Yang Memiliki Noise Pada Desain Spanduk";
-
-function casefolding(lower) {
-    console.log(lower.toLowerCase());
-    return lower.toLowerCase();
-}
-
-casefolding(judul);
